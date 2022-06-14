720bc02f7729302888ceb62ec7e56f3d0083ebe2
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-15 00:25:10 +0200

    Create the Api::V1::HelloworldsController controller

---
+++ test/controllers/api/v1/helloworld_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class Api::V1::HelloworldControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_helloworld_index_url
+    assert_response :success
+  end
+end
